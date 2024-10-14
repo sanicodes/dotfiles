ca68523292930b535793b4a42bf8c095f43b902d
Joshua Sanico <joshua@bposeats.com>
2024-10-14 11:28:29 +0800

    add markdownpreview.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -905,6 +905,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.colorschemes',
+  require 'kickstart.plugins.markdownpreview',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
