2693bfc9d5c03bf9d6d20ec9ca79ff7904a4c378
Joshua Sanico <joshua.bposeats@gmail.com>
2025-09-22 16:30:10 +0800

    update on lspconfig deprecation call

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -212,11 +212,13 @@ return {
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+
+            -- Define/override config
+            vim.lsp.config(server_name, server)
+
+            -- Enable server
+            vim.lsp.enable(server_name)
           end,
         },
       }
