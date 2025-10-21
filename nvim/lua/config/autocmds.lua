06bd4324987116940b73ac2167a42801f95de940
sanicodes <sanico.josh.g@gmail.com>
2025-10-21 12:15:08 +0800

    add live-preview and mjml syntax support

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,3 +5,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- compiles mjml files. Requires MJML to be manually installed.
+vim.api.nvim_create_user_command('MjmlCompile', function()
+  local current_file = vim.fn.expand '%:p'
+  if current_file:match '%.mjml$' then
+    local output_file = current_file:gsub('%.mjml$', '.html')
+    vim.cmd('!mjml ' .. current_file .. ' -o ' .. output_file)
+    print('Compiled ' .. current_file .. ' to ' .. output_file)
+  else
+    print 'Not an MJML file.'
+  end
+end, { desc = 'Compile current MJML file to HTML' })
+
+-- treats mjml files like html files since TS does not support it yet
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.mjml',
+  callback = function()
+    vim.bo.filetype = 'html'
+  end,
+})
