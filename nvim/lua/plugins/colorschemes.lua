473df736a78cb2a35c4489f487bfc642f5b3c759
Joshua Sanico <joshua.bposeats@gmail.com>
2025-11-05 18:09:22 +0800

    update colorscheme config

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -39,8 +39,6 @@ return {
       -- vim.o.background = 'light' -- or 'light' for light mode
       vim.g.gruvbox_material_background = 'medium'
       vim.g.gruvbox_material_foreground = 'original'
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_enable_bold = 1
       vim.g.gruvbox_material_cursor = 'auto'
       vim.g.gruvbox_material_dim_inactive_windows = 1
       vim.cmd.colorscheme 'gruvbox-material'
